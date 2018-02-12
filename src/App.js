02620f0cd1ce791ac4d5109ea4c62250c3babd53
ajd2144 <andrew.damico@northwestern.edu>
2018-02-12 11:03:10 -0600

    Starter react app.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>This is where the website components will go.</div>
+    );
+  }
+}
+
+export default App;
