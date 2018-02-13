60673a9913efe4fec824fe224fdedb1f028e0dca
Muriel <muriel.mangubat@gmail.com>
2018-02-12 21:47:35 -0600

    added product image table

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -7,5 +7,6 @@ lastName VARCHAR (25) NOT NULL,
 email VARCHAR(100) NOT NULL,
 passcode VARCHAR(41) NOT NULL COLLATE utf8_bin,
 image VARCHAR(32) null, 
-createTs TIMESTAMP NOT NULL default current_timestamp
+createTs TIMESTAMP NOT NULL default current_timestamp,
+updateTs TIMESTAMP NOT NULL default current_timestamp
 );
