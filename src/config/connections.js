d0596d7ddf8d4f8ed8b76e19c1ae8d4314fd2774
ajd2144 <andrew.damico@northwestern.edu>
2018-02-13 08:11:00 -0600

    Added the drop table sql file. Added the connection file for mysql.

---
+++ src/config/connections.js
@@ -0,0 +1,38 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+if (process.env.CLEARDB_DATABASE_URL) {
+  pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+} else {
+  var pool = mysql.createPool({
+    port: 3306,
+    host: "localhost",
+    user: "jb_user",
+    password: "jb_1234",
+    database: "jambid_db",
+    connectionLimit: 5
+  });
+};
+
+// Make connection.
+var getConnection = function(){
+  return new Promise(function(resolve, reject){
+    pool.getConnection(function(error, connection){
+      if(error){
+        console.error("error connecting: " + error);
+        return reject();
+      }
+
+      return resolve(connection);
+    });
+  });
+}
+
+//closes the connection
+var closeConnection = function(connection){
+  connection.release();
+}
+
+// Export connection for our ORM to use.
+module.exports = {getConnection: getConnection,
+  closeConnection: closeConnection};
