146b55baa457d79a1b6addcaafbb4190d59bdd12
Jamal <faizanjamal2018@u.northwestern.edu>
2018-02-15 18:45:38 -0600

    Resolve merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Nav from './components/Navbar';
 
+// future routes
+import Homepage from './components/Homepage';
+import Account from './components/Account';
+import Product from './components/Product';
+
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <Homepage />
+        <Account />
+        <Product />
       </div>
     );
   }
