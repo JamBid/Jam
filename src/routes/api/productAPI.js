23f77f3e08ab2e6e73b16d71e5510a1fc72d1b24
ajd2144 <andrew.damico@northwestern.edu>
2018-02-17 18:17:31 -0600

    Added the code to update the prod, but left it commentted for now.

--- src/routes/api/productAPI.js
+++ src/routes/api/productAPI.js
@@ -8,9 +8,20 @@ router.route('/:id')
             res.send(results)
         });
     })
-    .put(function(req, res){
-           //db.products.updateOne()
-           res.send(req.body.price)
-    });
+    /*.put(function(req, res){
+           db.products.updateOne(
+               {
+                   'startingPrice':req.body.startingPrice,
+                   'prodName': req.body.prodName,
+                   'description': req.body.description,
+                   'location': req.body.location,
+                   'category': req.body.category
+                },
+                req.params.id
+           )
+           .then(function(result){
+                res.send(result)
+           });           
+    });*/
 
 module.exports = router;
