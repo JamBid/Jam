666cbd14c799ee9f87781731ea0b586b3efd5cb5
ajd2144 <andrew.damico@northwestern.edu>
2018-02-17 20:37:53 -0600

    Added in the api route for inserting a new product.

--- src/controllers/productAPI.js
+++ src/controllers/productAPI.js
@@ -6,6 +6,10 @@ router.route('/:id')
         db.prod_prodImages.selectOneWithAllImage(req.params.id)
         .then(function(results){
             res.send(results)
+        })
+        .catch(function(error){
+            console.log(error);
+            res.send({status:'error'});
         });
     })
     /*.put(function(req, res){
@@ -21,7 +25,40 @@ router.route('/:id')
            )
            .then(function(result){
                 res.send(result)
-           });           
+           })
+           .catch(function(error){
+                console.log(error);
+                res.send({status:'error'});
+            });
     });*/
+
+router.route('/')
+    .post(function(req, res){
+        let images = [];
+
+        for(let i = 1; i <= 4; i++){
+            if(req.body["image"+i])
+                images.push({
+                    'image':req.body["image"+i],
+                    'imageType':req.body["image"+i+type]
+                });
+        }
+        
+        db.prod_prodImages.insertNewProd(
+            ['prodName','category','description','startingPrice','location',
+                'endTimestamp','sellerId'],
+            ['productId','image','imageType'],
+            [req.body.prodName,req.body.category,req.body.description,req.body.startingPrice,req.body.location,
+                req.body.endTimestamp,req,body.sellerId],
+            images
+        )
+        .then(function(result){
+            res.send(result);
+        })
+        .catch(function(error){
+            console.log(error);
+            res.send({status:'error'});
+        });
+    })
 
 module.exports = router;
