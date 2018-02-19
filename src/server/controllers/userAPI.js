986686d1831eb042a582fa7090ba6d60dc24348c
Andrew Damico <Leingod2005@gmail.com>
2018-02-19 08:04:49 -0600

    Update userAPI.js
    Taking the comment out as it is trying to refer to a variable that will not be there, i.e. name.

--- src/server/controllers/userAPI.js
+++ src/server/controllers/userAPI.js
@@ -16,7 +16,6 @@ router.route('/:id')
 //inserts a new user
 router.route('/')
     .post(function(req, res){
-        console.log(req.body.name);
         db.users.insertOne(
             ["email","userName","firstName","lastName","password","image","imageType"],
             [req.body.email, req.body.userName, req.body.firstName, req.body.lastName, req.body.password, req.body.image, req.body.imageType])
