59efdb77505edbea15947808fa42d7e93986a458
ajd2144 <andrew.damico@northwestern.edu>
2018-02-26 10:39:47 -0600

    Added logic on the search page to provide headers of categories.

--- src/components/Searchpage/Searchpage.jsx
+++ src/components/Searchpage/Searchpage.jsx
@@ -57,17 +57,26 @@ class Searchpage extends Component {
                 <div className="container ">
                     <div className="d-flex align-content-between flex-wrap justify-content-center">
                         {this.state.products.map((p,i) => (
-                            <div key={i} className="p-4 my-flex-item">
-                                <Link to={`/product/${p.id}`} >
-                                    <div className="card rounded-circle border-dark">
-                                        <div className="card-body home-category text-center">
-                                            <img className="imgCard" src={p.images[0].image} alt=""/>
-                                            <p>{p.prodName}</p>
+                            i === 0 || p.category !== this.state.products[i-1].category ?
+                                <div className="row">
+                                    <h3>{p.category}</h3>
+                                    {this.state.products.map((p2,j) => (
+                                        p2.category === p.category ?
+                                        <div key={j} className="p-4 my-flex-item">
+                                            <Link to={`/product/${p.id}`} >
+                                                <div className="card rounded-circle border-dark">
+                                                    <div className="card-body home-category text-center">
+                                                        <img className="imgCard" src={p.images[0].image} alt=""/>
+                                                        <p>{p.prodName}</p>
+                                                    </div>
+                                                </div>
+                                            </Link>
                                         </div>
-                                    </div>
-                                </Link>
-                            </div>
-                        ))}                    
+                                    :null))}
+                                </div>
+                            :
+                            null
+                        ))}
                     </div>
                 </div>
             </div>
